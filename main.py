822e4e56ca0a80ed1cd3b5cb194b2421bf37e044
ibrahim4529 <ibrahim.hanif4529@gmail.com>
2017-07-25 07:44:42 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, jsonify, url_for
+import aiml
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template('chat.html')
+@app.route('/chat')
+def chat():
+	return render_template('chat2.html')
+
+@app.route("/ask", methods=['POST','GET'])
+def ask():
+	message = str(request.form['chatmessage'])
+
+	kernel = aiml.Kernel()
+
+	if os.path.isfile("bot_brain.brn"):
+	    kernel.bootstrap(brainFile = "bot_brain.brn")
+	else:
+	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+	    kernel.saveBrain("bot_brain.brn")
+
+	# kernel now ready for use
+	while True:
+	    if message == "quit":
+	        exit()
+	    elif message == "save":
+	        kernel.saveBrain("bot_brain.brn")
+	    else:
+	        bot_response = kernel.respond(message)
+	        # print bot_response
+	        return jsonify({'status':'OK','answer':bot_response})
+
+if __name__ == "__main__":
+    app.run(debug=True)
