dc93a8e13715a32159037e0f7cdde4edcd0249cc
irfanhanandra <33511408+irfanandratama@users.noreply.github.com>
2018-06-28 12:16:03 +0700

    Changing AIML initialization
    AIML Kernel initialization must be outside function ask so it won't re-initialize everytime there are input from user. Make topic tag and set tag works in Flask.

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ import os
 
 app = Flask(__name__)
 
+kernel = aiml.Kernel()
+
+	if os.path.isfile("bot_brain.brn"):
+	    kernel.bootstrap(brainFile = "bot_brain.brn")
+	else:
+	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
+	    kernel.saveBrain("bot_brain.brn")
+
 @app.route("/")
 def hello():
     return render_template('chat.html')
@@ -17,14 +25,6 @@ def we():
 @app.route("/ask", methods=['POST','GET'])
 def ask():
 	message = str(request.form['chatmessage'])
-
-	kernel = aiml.Kernel()
-
-	if os.path.isfile("bot_brain.brn"):
-	    kernel.bootstrap(brainFile = "bot_brain.brn")
-	else:
-	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
-	    kernel.saveBrain("bot_brain.brn")
 
 	# kernel now ready for use
 	while True:
