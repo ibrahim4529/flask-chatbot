5b9580f6303f56693a6b196501b8bcb4ebb69e0c
Ibrahim Hanif <30402431+ibrahim4529@users.noreply.github.com>
2018-10-02 21:11:20 +0700

    Update app.py
    change initialization and function

--- app.py
+++ app.py
@@ -2,15 +2,21 @@ from flask import Flask, render_template, request, jsonify, url_for
 import aiml
 import os
 
-app = Flask(__name__)
 kernel = aiml.Kernel()
-def save(force):
-	if force == True:
+
+def load_kern(forcereload):
+	if os.path.isfile("bot_brain.brn") and not forcereload:
+		kernel.bootstrap(brainFile= "bot_brain.brn")
+	else:
+		kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
 		kernel.saveBrain("bot_brain.brn")
 
+
+app = Flask(__name__)
 @app.route("/")
 def hello():
-    return render_template('chat.html')
+	load_kern(False)
+	return render_template('chat.html')
 @app.route('/chat')
 def chat():
 	return render_template('chat2.html')
@@ -21,25 +27,22 @@ def we():
 @app.route("/ask", methods=['POST','GET'])
 def ask():
 	message = str(request.form['chatmessage'])
-	
-	if os.path.isfile("bot_brain.brn"):
-	    kernel.bootstrap(brainFile = "bot_brain.brn")
-	else:
-	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
-	    kernel.saveBrain("bot_brain.brn")
-	
 	if message == "save":
-	    save(True)
-	    return jsonify({'status':'OK','answer':"Saved"})
-	elif message == "quit":	
-	    exit()
-		
+	    kernel.saveBrain("bot_brain.brn")
+	    return jsonify({"status":"ok", "answer":"Brain Saved"})
+	elif message == "reload":
+		load_kern(True)
+		return jsonify({"status":"ok", "answer":"Brain Reloaded"})
+	elif message == "quit":
+		exit()
+		return jsonify({"status":"ok", "answer":"exit Thank You"})
 
 	# kernel now ready for use
-	while True:
+	else:
+		# while True:
 		bot_response = kernel.respond(message)
-	        # print bot_response
-	        return jsonify({'status':'OK','answer':bot_response})
+		# print bot_response
+		return jsonify({'status':'OK','answer':bot_response})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=False)
