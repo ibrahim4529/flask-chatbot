bd9b0f3258f44da160e819fd24952412d5ded591
Ibrahim Hanif <30402431+ibrahim4529@users.noreply.github.com>
2018-10-02 09:39:13 +0700

    Update app.py
    Update App.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import aiml
 import os
 
 app = Flask(__name__)
+kernel = aiml.Kernel()
+def save(force):
+	if force == True:
+		kernel.saveBrain("bot_brain.brn")
 
 @app.route("/")
 def hello():
@@ -17,23 +21,23 @@ def we():
 @app.route("/ask", methods=['POST','GET'])
 def ask():
 	message = str(request.form['chatmessage'])
-
-	kernel = aiml.Kernel()
-
+	
 	if os.path.isfile("bot_brain.brn"):
 	    kernel.bootstrap(brainFile = "bot_brain.brn")
 	else:
 	    kernel.bootstrap(learnFiles = os.path.abspath("aiml/std-startup.xml"), commands = "load aiml b")
 	    kernel.saveBrain("bot_brain.brn")
+	
+	if message == "save":
+	    save(True)
+	    return jsonify({'status':'OK','answer':"Saved"})
+	elif message == "quit":	
+	    exit()
+		
 
 	# kernel now ready for use
 	while True:
-	    if message == "quit":
-	        exit()
-	    elif message == "save":
-	        kernel.saveBrain("bot_brain.brn")
-	    else:
-	        bot_response = kernel.respond(message)
+		bot_response = kernel.respond(message)
 	        # print bot_response
 	        return jsonify({'status':'OK','answer':bot_response})
 
